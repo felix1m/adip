7f087e525013c370ba393d1801a471f358ff324b
Felix Müller <mueller4@tuba.informatik.uni-bonn.de>
2014-11-04 15:47:14 +0100

    solved 03: 1 and 2

---
+++ 03/2_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int summeTeiler(int n) {
+    
+    int sum = 0;
+    for (int i = 1; i <= n/2; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main(int argc, const char * argv[]) {
+
+    int n = 1;
+
+    printf("Gebe alle perfekten Zahlen kleiner n aus.\nBitte gebe eine positive Zahl n ein: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i < n; i++) {
+        int sum = summeTeiler(i);
+        
+        if (sum == i) { 
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
+
+
