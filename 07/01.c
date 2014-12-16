53a45984e84b6223433343f7b9c09fc12c97c099
Felix Müller <mueller.fe@gmail.com>
2014-12-16 12:23:01 +0100

    07: fix upper case in 01

--- 07/01.c
+++ 07/01.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *stringToLower(char *string, unsigned long len) {
+  int i;
+  for(i=0; i<len; i++) {
+    if(string[i] >= 'A' && string[i] <= 'Z') {
+      string[i] += 32;
+    }
+  }
+  return string;
+}
 
 int isPalindrome(char *buffer, unsigned long len) {
 
   if (len == 0) {return 0;}
   if (len == 1) {return 1;}
 
-
   int result = 1;
   int i;
 
+  buffer = stringToLower(buffer, len);
   unsigned long max = len / 2;
   len -= 1;
   for (i = 0; i < max; i++) {
-    if (buffer[i] != buffer [len-i]) {
+    if (buffer[i] != buffer[len-i]) {
       result = 0;
       break;
     }
   }
   return result;
 }
+
 
 
 int main(int argc, char *argv[]) {
