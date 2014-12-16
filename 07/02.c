da5d12e3645b368d374afb49a900b185c48c5173
Felix Müller <mueller.fe@gmail.com>
2014-12-16 13:13:06 +0100

    07: fix stupid mistakes in 02

--- 07/02.c
+++ 07/02.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <string.h>
 
 typedef struct {
-  char nachname[30];
+  char name[30];
   float note;
 } Student;
-
-
-Student *createStudent(char nachname, float note){
-  struct Student *tmp;
-    tmp = malloc(sizeof(Student));
-    if (tmp == NULL) {
-      printf("no memory!\n");
-      return NULL;
-    }
-
-    tmp->nachname = nachname;
-    tmp->note = note;
-
-    return tmp;
-}
 
 
 void printBestGrade(Student* students, unsigned int len) {
@@ -29,80 +13,67 @@ void printBestGrade(Student* students, unsigned int len) {
   float tmp;
   for (int i = 0; i < len; ++i)
   {
-    tmp = students[i]->note;
+    tmp = students[i].note;
     if (tmp < max) {
       max = tmp;
     }
   }
 
-  print("Beste note: \n");
+  printf("Beste note: \n");
 
   for (int i = 0; i < len; ++i)
   {
-    tmp = students[i]->note;
+    tmp = students[i].note;
     if (tmp == max) {
-      printf("Name: %s, Note: %f \n", students[i]->nachname, students[i]->grade);
+      printf("Name: %s, Note: %f \n", students[i].name, students[i].note);
     }
   }
 }
 
-void averageGrade(Student* students, unsigned int len) {
+float averageGrade(Student* students, unsigned int len) {
 
   double sum = 0;
 
   for (int i = 0; i < len; ++i)
   {
-    sum += students[i]->note;
+    sum += students[i].note;
   }
 
   return sum / len;
-
 }
 
 int main(int argc, char *argv[]) {
-    unsigned int numStudents;
+  unsigned int numStudents;
 
-    printf("Anzahl studenten:\n");
-    scanf("%d", &numStudents);
+  printf("Anzahl studenten:\n");
+  scanf("%d", &numStudents);
 
-    Student* students = malloc(numStudents * sizeof *Student);
-    for (int i = 0; i < numStudents; ++i)
-    {
-      printf("Enter Name:\n");
-      char *name = NULL;
-      int read;
-      unsigned int len;
-      float note;
+  Student* students = malloc(numStudents * sizeof(Student));
+  for (int i = 0; i < numStudents; ++i)
+  {
+    char name[30];
+    float note;
 
-      printf("Name eingeben:\n");
+    printf("Name eingeben:\n");
+    scanf("%s", name);
 
-      read = getline(&name, &len, stdin);
-      while (-1 == read)
-          printf("No line read...\n");
-          read = getline(&name, &len, stdin);
+    printf("Note eingeben:\n");
+    scanf("%f", &note);
 
+    while (note < 1.0 || note > 5.0) {
+      printf("Ungültige note!\n");
       printf("Note eingeben:\n");
       scanf("%f", &note);
-
-      while (note < 1.0 || note > 5.0) {
-        printf("Ungültige note!\n");
-        printf("Note eingeben:\n");
-        scanf("%f", &note);
-      }
-
-      students[i] = createStudent();
-
-      while (!students[i])
-          printf("No line read...\n");
-          read = getline(&name, &len, stdin);
-
-      students[i] = createStudent();
     }
 
-    printBestGrade(students, numStudents);
+    students[i].note=note;
+    strcpy(students[i].name, name);
+  }
 
-    printf("Durchschnittsnote: %f\n", averageGrade(students, numStudents));
+  printBestGrade(students, numStudents);
 
-    free(students);
-    return 0;
+  printf("Durchschnittsnote: %f\n", averageGrade(students, numStudents));
+
+  free(students);
+  return 0;
 }
